6bfc88c5e292d8d4f636776f27f0c4ae3fd6a057
Mikhail Yudin <fagci.nsk@gmail.com>
2021-09-10 23:34:08 +0700

    show response body flag

--- gmf.py
+++ gmf.py
@@ -2,12 +2,12 @@
 """Global Misconfig Finder"""
 from argparse import ArgumentParser
 from functools import cached_property
-from http.client import HTTPConnection
+from http.client import BadStatusLine, HTTPConnection
 from ipaddress import IPV4LENGTH, IPv4Address
 from queue import Queue
 from random import randrange
 import re
-from socket import setdefaulttimeout
+from socket import setdefaulttimeout, timeout as STimeout
 import sys
 from threading import Event, Thread
 
@@ -15,36 +15,54 @@ MAX_IPV4 = 1 << IPV4LENGTH
 
 
 class Checker(Thread):
-    def __init__(self, q: Queue, r: Event, p: str, exclude: str):
+    def __init__(self, q: Queue, r: Event, path, exclude, proxy, show_body):
         super().__init__(daemon=True)
         self._q = q
         self._r = r
-        self._p = p
+        self._p = path
+        self._proxy = proxy
+        self._sb = show_body
         self._ex = re.compile(exclude)
 
     def check(self, ip):
         try:
             c = HTTPConnection(ip)
+            if self._proxy:
+                ph, pp = self._proxy.split(':')
+                c.set_tunnel(ph, int(pp))
             c.request('GET', self.rand_path)
             r = c.getresponse()
-            r.read()
+            r.close()
             if 100 <= r.status < 300:
-                return False
+                return False, ''
             c.request('GET', self._p)
             r = c.getresponse()
-            text = r.read().decode(errors='ignore').lower()
+            text = r.read().decode(errors='ignore')
             c.close()
-            return 100 <= r.status < 300 and not self._ex.match(text)
-        except:
-            return False
+            ok = 100 <= r.status < 300 and not self._ex.findall(text.lower())
+            return ok, text
+        except OSError as e:
+            if str(e).startswith('Tunnel'):
+                print(e)
+                self._r.clear()
+        except (STimeout, ConnectionRefusedError, ConnectionRefusedError,
+                BadStatusLine):
+            pass
+        except Exception as e:
+            print(repr(e))
+
+        return False, ''
 
     def run(self):
         while self._r.is_set():
             ip = self._q.get()
             if not ip:
                 break
-            if self.check(ip):
+            result, body = self.check(ip)
+            if result:
                 print(ip)
+                if self._sb:
+                    print(body, end='\n_______________\n')
                 if not sys.stdout.isatty():
                     sys.stderr.write(f'{ip}\n')
 
@@ -73,7 +91,7 @@ class Generator(Thread):
                 self._q.put(str(ip_address))
 
 
-def main(path, workers, timeout, limit, exclude):
+def main(path, workers, timeout, limit, exclude, proxy, show_body):
     sys.stderr.write('--=[ G M F ]=--\n')
     threads = []
     queue = Queue(workers * 3)
@@ -83,8 +101,9 @@ def main(path, workers, timeout, limit, exclude):
 
     try:
         running.set()
+
         for _ in range(workers):
-            t = Checker(queue, running, path, exclude)
+            t = Checker(queue, running, path, exclude, proxy, show_body)
             threads.append(t)
             t.start()
 
@@ -119,6 +138,8 @@ if __name__ == '__main__':
     ap.add_argument('-w', '--workers', type=int, default=512)
     ap.add_argument('-t', '--timeout', type=float, default=0.75)
     ap.add_argument('-l', '--limit', type=int, default=1000000)
+    ap.add_argument('--proxy', type=str, default='')
+    ap.add_argument('-b','--show-body', default=False, action='store_true')
     ap.add_argument('-x',
                     '--exclude',
                     type=str,
